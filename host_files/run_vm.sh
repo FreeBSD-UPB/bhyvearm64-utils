c141287a9caa2aa8dc9d3e8cd9a2ebf3772cf7f7
Alexandru Elisei <alexandru.elisei@gmail.com>
2018-07-05 13:06:28 +0300

    host_files/run_vm.sh: Fix syntax error

--- host_files/run_vm.sh
+++ host_files/run_vm.sh
@@ -1,10 +1,6 @@
 #!/bin/sh -x
 
-if [ "$#" = "0" ]; then
-	VMNAME="test"
-else
-	VMNAME="$1"
-fi
+[ "$#" = "0" ] && set VMNAME="test" || set VMNAME="$1"
 
 echo "[Loading kernel module 'vmm']"
 kldload vmm
@@ -13,7 +9,7 @@ echo "[Creating VM '$VMNAME' from kernel image: kernel.bin]"
 bhyveload -k kernel.bin $VMNAME
 
 if [ "$?" = "0" ]; then
-	echo "[Starting VM '$VMNAME' with bvmconsole]"
+	echo "[Starting VM '$VMNAME' with: bvmconsole]"
 	bhyve -e 0x80000000UL -m 128MB -b $VMNAME
 else
 	echo "[VM aborted]"
