1df1f2bc7eeb3e3f9f15bf1cb8507807e94b923e
Alexandru Elisei <alexandru.elisei@gmail.com>
2018-12-23 19:24:31 +0000

    run_image.sh: Use bridged connection for the emulated OS

--- run_image.sh
+++ run_image.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-MODEL=/home/alex/data/bhyvearm64/Foundation_Platformpkg/models/Linux64_GCC-4.9/Foundation_Platform
+MODEL=/home/alex/data/bhyvearm64/Foundation_Platform_11.4/Foundation_Platformpkg/models/Linux64_GCC-4.9/Foundation_Platform
 
-BL1=/home/alex/data/bhyvearm64/uefi/bl1.bin
-FIP=/home/alex/data/bhyvearm64/uefi/fip.bin
+BL1=/home/alex/data/bhyvearm64/uefi/bck/bl1.bin
+FIP=/home/alex/data/bhyvearm64/uefi/bck/fip.bin
 DISK_DIR=/home/alex/data/bhyvearm64/disk
 DISK_NAME="disk.img"
 DISK="${DISK_DIR}/${DISK_NAME}"
@@ -16,6 +16,8 @@ $MODEL  --cores=1 \
         --use-real-time \
         --arm-v8.0 \
         --gicv3 \
+        --network=bridged \
+        --network-bridge=tap0 \
         --data=${BL1}@0x0 \
         --data=${FIP}@0x8000000 \
         --block-device=${DISK} \
